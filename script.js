2081b3a9496e4bdf387303055af6854cdfbd3eb1
pavel <leskovecpavel7@gmail.com>
2018-10-16 19:36:21 +0300

    forAlex

---
+++ script.js
@@ -0,0 +1,83 @@
+var army;
+
+function DOT(obj, prop) {
+	if(typeof obj !== "object" ||  typeof prop !== "string") {
+		console.log("Error");
+	}
+	else {
+		return DOT
+	}
+}
+var petrSidorov = {
+	name: petr,
+	sex: male,
+	age: 20,
+	currentMoney: 77.25,
+	listExam: {
+			physics: 7,
+				geography: 8,
+					mathematics: 8
+		}
+	
+}
+var vanyaIvanov = {
+	name: vanya,
+	sex: male,
+	age: 19,
+	currentMoney: 25.89,
+	listExam: {
+			physics: 6,
+				geography: 5,
+					mathematics: 5
+		}
+	
+}
+	var vanyaIvanov = {
+	name: vanya,
+	sex: male,
+	age: 19,
+	currentMoney: 35.89,
+	listExam: {
+			physics: 6,
+				geography: 5,
+					mathematics: 5
+		}
+}
+	var annPetrova = {
+	name: ann,
+	sex: female,
+	age: 19,
+	currentMoney: 15.89,
+	listExam: {
+			physics: 9,
+				geography: 10,
+					mathematics: 9
+		}
+}
+	function armyNow () {
+		if (army === true)			{ 
+			console.log(obj.name + " welcomeToHell")
+			} 
+		else {
+			console.log(obj.name + " Congratulations, welcome to next course")
+		}
+		return army
+	}
+	function mediumMark(annPetrova) {
+	var studentMark =	(obj.listExam.physics + obj.listExam.geography + obj.listExam.mathematics) / 3;
+		return studentMark;
+	}
+function testMediumMark() {
+	if(studentMark >= 7) {
+		console.log(obj.name + " Congratulations, welcome to next course");
+		
+		console.log(toFixed() prop.currentMoney + 3.864)
+	}
+	if(army === true) {
+		
+		console.log(obj.name + " welcome to army")
+	}
+	if(army === false) {
+		console.log(obj.name + "welcome to reexam")
+	}
+}
