311480365973755fa89107ed041be18f274935a4
pavel-leskavets <43208543+pavel-leskavets@users.noreply.github.com>
2018-10-24 17:58:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,51 +11,57 @@ var petrSidorov = {
 	sex: "male",
 	age: 20,
 	currentMoney: 77.25,
-	listExam: {
-			physics: 7,
-				geography: 8,
-					mathematics: 8
-		}
-	
+	listExam: [
+		{examStudents: 7},
+		{examStudents: 8},
+		{examStudents: 8}
+		],
+	getAverageGrade: petrSidorovMediumMark,
+	checkIfValidForArmy: petrSidorovCheckToArmy
+
 }
 var vanyaIvanov = {
 	name: "vanya",
 	sex: "male",
 	age: 19,
 	currentMoney: 25.89,
-	listExam: {
-			physics: 6,
-				geography: 5,
-					mathematics: 5
-		}
-	
+	listExam:[ 
+	{examStudents: 6},
+	{examStudents: 5},
+	{examStudents: 5}
+		],
+	getAverageGrade: vanyaIvanovMediumMark,
+	checkIfValidForArmy: vanyaIvanovCheckToArmy
 }
-	
-
 	var annPetrova = {
 	name: "ann",
 	sex: "female",
 	age: 19,
 	currentMoney: 15.89,
-		listExam: {
-			physics: 9,
-				geography: 10,
-					mathematics: 9
-		}
+		listExam: [
+			{examStudents: 9},
+			 {examStudents: 10},
+			 {examStudents: 9}
+		],
+	getAverageGrade: annPetrovaMediumMark,
+	checkIfValidForArmy: annPetrovaCheckToArmy
 		
 }
+	var firstGradeStudents = [];
+firstGradeStudents.push(annPetrova, petrSidorov, vanyaIvanov);
+console.log(firstGradeStudents);
 	var minAgeForArmy = 18;
   var maxAgeForArmy = 27;
   var GENDER = "male"
-	function checkValidForService (obj) {
+	function checkValidForService (firstGradeStudents) {
 		
-		if (obj.age >= minAgeForArmy && obj.age < maxAgeForArmy && obj.sex === GENDER)			{ 
-			console.log(obj.name + " welcomeToHell");
+		if (firstGradeStudents.age >= minAgeForArmy && firstGradeStudents.age < maxAgeForArmy && firstGradeStudents.sex === GENDER)			{ 
+			console.log(firstGradeStudents.name + " welcomeToHell");
 			return true;
 			} 
-		else if(obj.sex === "female") {
+		else if(firstGradeStudents.sex === "female") {
 			
-			console.log(obj.name + " You are a woman");
+			console.log(firstGradeStudents.name + " You are a woman");
 			return false;
 		}
 		else {
@@ -64,31 +70,65 @@ var vanyaIvanov = {
 		}
 		
 	}
+var annPetrovaCheckToArmy = checkValidForService(annPetrova); 
+var vanyaIvanovCheckToArmy = checkValidForService(vanyaIvanov);
+var petrSidorovCheckToArmy = checkValidForService(petrSidorov);
 
-	function checkMediumMark(obj) {
-	var studentMark =	(obj.listExam.physics + obj.listExam.geography + obj.listExam.mathematics) / 3;
-		return studentMark;
-	}
-var annPetrovaMediumMark = checkMediumMark(annPetrova); 
-var vanyaIvanovMediumMark = checkMediumMark(vanyaIvanov);
-var petrSidorovMediumMark = checkMediumMark(petrSidorov);
+	function checkMediumMark(listExam) {
+	var checkMediumMarkStudents = [];
+		for(var i = 0; i < listExam.length; i++){
+			checkMediumMarkStudents.push(listExam[i].examStudents);
+			
+			
+		}
+			var averageMark = 0;
+for(var i = 0; i < checkMediumMarkStudents.length; i++){
+	
+	averageMark += checkMediumMarkStudents[i];
+}
+	var averageMarkStudent = averageMark / checkMediumMarkStudents.length;
+		var averageMarkFix = averageMarkStudent.toFixed(2);
+console.log(averageMarkFix);
+		return parseFloat(averageMarkFix)
+		}
+		
+	
+		
+	
+	
+var annPetrovaMediumMark = checkMediumMark(annPetrova.listExam); 
+var vanyaIvanovMediumMark = checkMediumMark(vanyaIvanov.listExam);
+var petrSidorovMediumMark = checkMediumMark(petrSidorov.listExam);
  
 
-function checkToNextCourse(obj) {
-	if(checkMediumMark(obj) >= 7) {
-		console.log(obj.name + " Congratulations, welcome to next course" );
-		var newCurrentMoney = obj.currentMoney + 3.864;
-		console.log(obj.name + " Your new current money " + newCurrentMoney.toFixed(2))
+
+
+function checkToNextCourse(firstGradeStudents) {
+	if(checkMediumMark(firstGradeStudents.listExam) >= 7) {
+		console.log(firstGradeStudents.name + " Congratulations, welcome to next course" );
+		var newCurrentMoney = firstGradeStudents.currentMoney + 3;
+		console.log(firstGradeStudents.name + " Your new current money " + newCurrentMoney.toFixed(2))
 	}
 	else {
-		if(checkValidForService (obj) === true) {
-			console.log(obj.name + " Welcome to army");
+		if(checkValidForService (firstGradeStudents) === true) {
+			console.log(firstGradeStudents.name + " Welcome to army");
 		}
 		else {
-			console.log(obj.name + " You are a woman, see you  retake exam");
+			console.log(firstGradeStudents.name + " You are a woman, see you  retake exam");
 		}
 	}
+	
+
 }
-var annPetrovaCheckToNextCourse = checkToNextCourse(annPetrova); 
-var vanyaIvanovCheckToNextCourse = checkToNextCourse(vanyaIvanov);
-var petrSidorovCheckToNextCourse = checkToNextCourse(petrSidorov);
+function filterStudentsForArmy(firstGradeStudents) {
+	
+ 
+	if(checkMediumMark(firstGradeStudents.name) < 7 && checkValidForService(firstGradeStudents.name) === true ) {
+		return true
+	} else {
+		return false
+	}
+	
+	
+}
+	
