937f00d9294729099577b2c353cd2dc77a24c63c
pavel-leskavets <43208543+pavel-leskavets@users.noreply.github.com>
2018-10-28 17:30:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,210 +1,197 @@
-function DOT(obj, prop) {
-	if(typeof obj !== "object" ||  typeof prop !== "string" || typeof obj === null) {
-		console.log("Error");
-	}
-	else {
-		return obj[prop]
-	}
-}
+
+
+var firstCourseStudents = [];
+firstCourseStudents.push(student1, student2, student3, student4, student5, student6, student7)
+
+var genders = {
+	MALE: 'male',
+	FEMALE: 'female'
+};
+var MIN_VALID_YEAR_FOR_ARMY = 18; 
+var MAX_VALID_YEAR_FOR_ARMY = 27; 
+
+var MIN_AVERAGE_GRADE = 6; 
+
 var BENEFIT = 3.1415;
-var minAverageGrade = 7;
-var petrSidorov = {
-	id: 1,
-	name: "petr",
-	sex: "male",
-	age: 20,
-	currentMoney: 77.25,
-	listExam: [
-		{examStudents: 5},
-		{examStudents: 4},
-		{examStudents: 4}
-		],
-	getAverageGrade: petrSidorovMediumMark,
-	checkIfValidForArmy: petrSidorovCheckToArmy
+var student1 = { 
+id: 1, 
+name: "petr", 
+gender: genders.MALE, 
+age: 20, 
+course: 1,
+cashAmount: 77.25, 
+grades: [ 
+ {subject: 'russian', grade: 6},
+ {subject: 'history', grade: 9},
+ {subject: 'mathematics', grade: 8},
+ {subject: 'physics', grade: 5}
+], 
+getAverageGrade: getAverageGrade , 
+checkIfValidForArmy:  checkIfValidForArmy,
+changeCashAmount: changeCashAmount
 
-}
-var vanyaIvanov = {
-	id: 2,
-	name: "vanya",
-	sex: "male",
-	age: 19,
-	currentMoney: 25.89,
-	listExam:[ 
-	{examStudents: 6},
-	{examStudents: 5},
-	{examStudents: 5}
-		],
-	getAverageGrade: vanyaIvanovMediumMark,
-	checkIfValidForArmy: vanyaIvanovCheckToArmy
-}
-	var annPetrova = {
-		id: 3,
-	name: "ann",
-	sex: "female",
-	age: 19,
-	currentMoney: 15.89,
-		listExam: [
-			{examStudents: 9},
-			 {examStudents: 10},
-			 {examStudents: 9}
-		],
-	getAverageGrade: annPetrovaMediumMark,
-	checkIfValidForArmy: annPetrovaCheckToArmy
-		
-}
-var levTolstoi = {
-	id: 4,
-	name: "lev",
-	sex: "male",
-	age: 17,
-	currentMoney: 35.89,
-	listExam:[ 
-	{examStudents: 7},
-	{examStudents: 4},
-	{examStudents: 2}
-		],
-	getAverageGrade: levTolstoiMediumMark,
-	checkIfValidForArmy: levTolstoiCheckToArmy
-}
-var kolyaGogol = {
-	id: 5,
-	name: "nikolai",
-	sex: "male",
-	age: 19,
-	currentMoney: 44.45,
-	listExam:[ 
-	{examStudents: 6},
-	{examStudents: 8},
-	{examStudents: 6}
-		],
-	getAverageGrade: kolyaGogolMediumMark,
-	checkIfValidForArmy: kolyaGogolCheckToArmy
-}
-var mishanyaLermontov = {
-	id: 6,
-	name: "mishka_duelyant",
-	sex: "male",
-	age: 25,
-	currentMoney: 85.36,
-	listExam:[ 
-	{examStudents: 7},
-	{examStudents: 8},
-	{examStudents: 6}
-		],
-	getAverageGrade: mishanyaLermontovMediumMark,
-	checkIfValidForArmy: mishanyaLermontovCheckToArmy
-}
-var stivenKing = {
-	id: 7,
-	name: "stiven",
-	sex: "male",
-	age: 20,
-	currentMoney: 58.7,
-	listExam:[ 
-	{examStudents: 9},
-	{examStudents: 7},
-	{examStudents: 8}
-		],
-	getAverageGrade: stivenKingMediumMark,
-	checkIfValidForArmy: stivenKingCheckToArmy
-}
-	var firstGradeStudents = [];
-firstGradeStudents.push(annPetrova, petrSidorov, vanyaIvanov, levTolstoi, kolyaGogol, mishanyaLermontov, stivenKing);
-console.log(firstGradeStudents);
-	var minAgeForArmy = 18;
-  var maxAgeForArmy = 26;
-  var GENDER = "male"
-	function checkValidForService (firstGradeStudents) {
-		
-		if (firstGradeStudents.age >= minAgeForArmy && firstGradeStudents.age < maxAgeForArmy && firstGradeStudents.sex === GENDER )			{ 
-			
-			return true;
-			} 
-		else if(firstGradeStudents.sex === "female") {
-			
-			
-			return false;
-		}
-		else {
-			return false;
-		}
-		
+} 
+var student2 = { 
+id: 2, 
+name: "vanya", 
+gender: genders.MALE, 
+age: 19,
+course: 1,	
+cashAmount: 25.89, 
+grades:[ 
+{subject: 'russian', grade: 6},
+ {subject: 'history', grade: 6},
+ {subject: 'mathematics', grade: 8},
+ {subject: 'physics', grade: 7}
+], 
+getAverageGrade: getAverageGrade , 
+checkIfValidForArmy: checkIfValidForArmy,
+changeCashAmount: changeCashAmount
+} 
+var student3 = { 
+id: 3, 
+name: "ann", 
+gender: genders.FEMALE, 
+age: 19,
+course: 1,	
+cashAmount: 15.89, 
+grades: [ 
+{subject: 'russian', grade: 6},
+ {subject: 'history', grade: 3},
+ {subject: 'mathematics', grade: 5},
+ {subject: 'physics', grade: 6}
+], 
+getAverageGrade: getAverageGrade , 
+checkIfValidForArmy: checkIfValidForArmy,
+changeCashAmount: changeCashAmount
+
+} 
+var student4 = { 
+id: 4, 
+name: "lev", 
+gender: genders.MALE,
+age: 17,
+course: 1,	
+cashAmount: 35.89, 
+grades:[ 
+{subject: 'russian', grade: 4},
+ {subject: 'history', grade: 9},
+ {subject: 'mathematics', grade: 8},
+ {subject: 'physics', grade: 7}
+], 
+getAverageGrade: getAverageGrade , 
+checkIfValidForArmy: checkIfValidForArmy,
+changeCashAmount: changeCashAmount 
+} 
+var student5 = { 
+id: 5, 
+name: "nikolai", 
+gender: genders.MALE, 
+age: 19,
+course: 1,
+cashAmount: 44.45, 
+grades:[ 
+{subject: 'russian', grade: 9},
+ {subject: 'history', grade: 9},
+ {subject: 'mathematics', grade: 9},
+ {subject: 'physics', grade: 8} 
+], 
+getAverageGrade: getAverageGrade , 
+checkIfValidForArmy: checkIfValidForArmy,
+changeCashAmount: changeCashAmount
+} 
+var student6 = { 
+id: 6, 
+name: "mishka_duelyant", 
+gender: genders.MALE, 
+age: 25, 
+course: 1,
+cashAmount: 85.36, 
+grades:[ 
+{subject: 'russian', grade: 4},
+ {subject: 'history', grade: 2},
+ {subject: 'mathematics', grade: 3},
+ {subject: 'physics', grade: 3}
+], 
+getAverageGrade: getAverageGrade , 
+checkIfValidForArmy: checkIfValidForArmy,
+changeCashAmount: changeCashAmount
+} 
+var student7 = { 
+id: 7, 
+name: "stiven", 
+gender: genders.MALE, 
+age: 20,
+course: 1,
+cashAmount: 58.7, 
+grades:[ 
+{subject: 'russian', grade: 6},
+ {subject: 'history', grade: 9},
+ {subject: 'mathematics', grade: 8},
+ {subject: 'physics', grade: 5}],
+getAverageGrade: getAverageGrade , 
+checkIfValidForArmy: checkIfValidForArmy,
+changeCashAmount: changeCashAmount
+} 
+
+var studentsPassedExams = getStudentsPassedExams(firstCourseStudents, MIN_AVERAGE_GRADE);
+
+var goodStudentsIds = studentsPassedExams.map(function(array) { 
+  return array.id 
+	});
+
+var excludedStudents = getExcludedStudents(firstCourseStudents, goodStudentsIds);
+var studentsForArmy = getStudentsForArmy(excludedStudents);
+var secondGradeStudents = getStudentsForNextCourse(getStudentsPassedExams);
+function getAverageGrade(firstCourseStudents) {
+	var sum = 0;
+	for(var i = 0; i < this.grades.length; i++) {
+		sum += this.grades[i].subject
 	}
-var annPetrovaCheckToArmy = checkValidForService(annPetrova); 
-var vanyaIvanovCheckToArmy = checkValidForService(vanyaIvanov);
-var petrSidorovCheckToArmy = checkValidForService(petrSidorov);
-var levTolstoiCheckToArmy = checkValidForService(levTolstoi);
-var kolyaGogolCheckToArmy = checkValidForService(kolyaGogol);
-var mishanyaLermontovCheckToArmy = checkValidForService(mishanyaLermontov);
-var stivenKingCheckToArmy = checkValidForService(stivenKing);
-
-	function checkMediumMark(listExam) {
-	var checkMediumMarkStudents = [];
-		for(var i = 0; i < listExam.length; i++){
-			checkMediumMarkStudents.push(listExam[i].examStudents);		
-		}
-			var averageMark = 0;
-    for(var i = 0; i < checkMediumMarkStudents.length; i++){	
-	   averageMark += checkMediumMarkStudents[i];
+	return sum / this.grades.length
 }
-	var averageMarkStudent = averageMark / checkMediumMarkStudents.length;
-	var averageMarkFix = averageMarkStudent.toFixed(2);
 
-		return parseFloat(averageMarkFix)	}
-			
-var annPetrovaMediumMark = checkMediumMark(annPetrova.listExam); 
-var vanyaIvanovMediumMark = checkMediumMark(vanyaIvanov.listExam);
-var petrSidorovMediumMark = checkMediumMark(petrSidorov.listExam);
-var levTolstoiMediumMark = checkMediumMark(levTolstoi.listExam);
-var kolyaGogolMediumMark = checkMediumMark(kolyaGogol.listExam);
-var mishanyaLermontovMediumMark = checkMediumMark(mishanyaLermontov.listExam);
-var stivenKingMediumMark = checkMediumMark(stivenKing.listExam);
-
-function checkToNextCourse(firstGradeStudents) {
-	if(checkMediumMark(firstGradeStudents.listExam) >= 7) {
-		console.log(firstGradeStudents.name + " Congratulations, welcome to next course" );
-		var newCurrentMoney = firstGradeStudents.currentMoney + 3;
-		console.log(firstGradeStudents.name + " Your new current money " + newCurrentMoney.toFixed(2))
-	}
-	else {
-		if(checkValidForService (firstGradeStudents) === true) {
-			console.log(firstGradeStudents.name + " Welcome to army");
-		}
-		else {
-			console.log(firstGradeStudents.name + " You are a woman, see you  retake exam");
-		}
-	}
-	
-}
-function filterStudentsForArmy(firstGradeStudents) { 
-	if(checkMediumMark(firstGradeStudents.name) < 7 && checkValidForService(firstGradeStudents.name) === true ) {
+function checkIfValidForArmy(firstCourseStudents) {
+	if(this.gender === 'female' ){
+		return false
+	} else if(this.age >= MIN_VALID_YEAR_FOR_ARMY && this.age < MAX_VALID_YEAR_FOR_ARMY){
 		return true
-	} else {
+	} else{
 		return false
 	}
-	
-	
 }
-function checkingToNextCourse (firstGradeStudents) {
-    return firstGradeStudents.filter((item) => checkMediumMark(item.listExam) >= 6);
+function changeCashAmount () {
+return parseFloat((this.cashAmount + BENEFIT).toFixed(2)) 
 }
 
-var secondGradeStudents = checkingToNextCourse(firstGradeStudents);
-secondGradeStudents.map(function(arrayItem) {
-    delete arrayItem.listExam;
-    arrayItem.currentMoney += parseFloat(BENEFIT.toFixed(2));
-    return arrayItem;
-});
-	console.log(secondGradeStudents);
-var goodStudentsIds = secondGradeStudents.map(function(arrayId) {
-		return arrayId.id
-})
- 
-console.log(goodStudentsIds);
+function getStudentsPassedExams(firstCourseStudents, MIN_AVERAGE_GRADE) {
+	return firstCourseStudents.filter(function(student) {
+		return (student.getAverageGrade() >= MIN_AVERAGE_GRADE);
+	})
+}
 
-function getExcludedStudents(firstGradeStudents, goodStudentsIds) {
-var excludedStudents = (firstGradeStudents.this.id).filter(n => goodStudentsIds.indexOf(n) === -1);
-
-	console.log(excludedStudents);
-	return excludedStudents
+function getExcludedStudents(students, goodStudentsIds) {
+	 var exludedStudents = [];
+    for (var i = 0; i < firstCourseStudents.length; i++) {
+        if (goodStudentsIds.indexOf(firstCourseStudents[i].id) === -1) {
+            exludedStudents.push(firstCourseStudents[i]);
+        }
+    }
+    return exludedStudents;
+}
+function getStudentsForArmy(firstCourseStudents) {
+	var studentsForArmy = []
+	firstCourseStudents.filter(function(arrayArmy) {
+		if(arrayArmy.checkIfValidForArmy() === true){
+			studentsForArmy.push(arrayArmy)
+		}
+	})
+	return studentsForArmy
+}
+function getStudentsForNextCourse(getStudentsPassedExams) {
+  return firstCourseStudents.map(function(arrayItem) {
+        arrayItem.course += 1;
+        arrayItem.cashAmount = arrayItem.changeCashAmount();
+        return arrayItem;
+    });
 }
